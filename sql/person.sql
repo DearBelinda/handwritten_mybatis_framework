1b1561d52b5e2dbf585bfd3f3f7aa3d99042e264
suhongwei <670892861@qq.com>
2018-06-21 17:32:04 +0800

    mybatis-v1.2：数据结果修改

--- sql/person.sql
+++ sql/person.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80011
 File Encoding         : 65001
 
-Date: 2018-06-20 19:11:36
+Date: 2018-06-21 17:31:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,5 +23,6 @@ CREATE TABLE `person` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `age` int(11) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
+  `sex` tinyint(4) DEFAULT '1' COMMENT '男：1 女 0；',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
